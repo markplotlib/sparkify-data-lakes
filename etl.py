bf36cde0b4b1a5d10821992af9c9701b8116657d
markplotlib <mark.chesney@gmail.com>
2021-01-18 12:44:29 -0800

    process_song_data: extract columns to create songs table

--- etl.py
+++ etl.py
@@ -5,6 +5,12 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
+# The ETL script reads song_data and load_data from S3,
+# transforms them to create five different tables,
+# and writes them to partitioned parquet files in table directories on S3.
+
+# Data subset
+# You can work on your project with a smaller dataset found in workspace, then move on to bigger dataset on AWS.
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
@@ -13,6 +19,8 @@ os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 
 
+# online docs
+# https://spark.apache.org/docs/latest/sql-getting-started.html
 def create_spark_session():
     spark = SparkSession \
         .builder \
@@ -22,21 +30,33 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+    reads two JSON files from S3 (song_data and load_data),
+    transforms them to create five different tables,
+    and outputs them back to S3 as partitioned parquet files (as fact and dimensional tables)
+
+    :param spark: an existing SparkSession
+    :param input_data: S3 bucket directory (e.g., "s3a://udacity-dend/")
+    :param output_data: empty string
+    """
     # get filepath to song data file
-    song_data = 
-    
+    song_data = input_data + 'song_data/A/A/B/*.json'                   # TEST
+    # song_data = os.path.join(input_data, "song-data/*/*/*/*.json")    # FINAL
+
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
-    
+    songs_table = df.select(df['song_id'], df['title'], df['artist_id'], df['year'], df['duration']).show()
+    # ^ ABOVE: style from here -- https://spark.apache.org/docs/latest/sql-getting-started.html
+    # songs_table = df['song_id', 'title', 'artist_id', 'year', 'duration']  # might this work instead?
+
     # write songs table to parquet files partitioned by year and artist
     songs_table
 
     # extract columns to create artists table
-    artists_table = 
-    
+    artists_table =
+
     # write artists table to parquet files
     artists_table
 
@@ -46,36 +66,36 @@ def process_log_data(spark, input_data, output_data):
     log_data =
 
     # read log data file
-    df = 
-    
-    # filter by actions for song plays
-    df = 
+    df =
 
-    # extract columns for users table    
-    artists_table = 
-    
+    # filter by actions for song plays
+    df =
+
+    # extract columns for users table
+    artists_table =
+
     # write users table to parquet files
     artists_table
 
     # create timestamp column from original timestamp column
     get_timestamp = udf()
-    df = 
-    
+    df =
+
     # create datetime column from original timestamp column
     get_datetime = udf()
-    df = 
-    
+    df =
+
     # extract columns to create time table
-    time_table = 
-    
+    time_table =
+
     # write time table to parquet files partitioned by year and month
     time_table
 
     # read in song data to use for songplays table
-    song_df = 
+    song_df =
 
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    # extract columns from joined song and log datasets to create songplays table
+    songplays_table =
 
     # write songplays table to parquet files partitioned by year and month
     songplays_table
@@ -85,8 +105,8 @@ def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = ""
-    
-    process_song_data(spark, input_data, output_data)    
+
+    process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)
 
 
